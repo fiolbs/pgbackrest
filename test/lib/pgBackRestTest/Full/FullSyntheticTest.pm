60cac5aafe80e802b6152b0fd5ff1410467017e5
David Steele <david@pgmasters.net>
2017-02-22 10:51:26 -0500

    Fix.

--- test/lib/pgBackRestTest/Full/FullSyntheticTest.pm
+++ test/lib/pgBackRestTest/Full/FullSyntheticTest.pm
@@ -414,9 +414,9 @@ sub run
         executeTest("sudo chmod g+w " . dirname($strTmpPath));
 
         testPathMove($oHostBackup->repoPath() . '/backup/' . $self->stanza() . "/${strFullBackup}", $strTmpPath);
-        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}");
+        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}") if $bRemote;
 
-        executeTest("sudo chmod g+w " . $strTmpPath);
+        executeTest("sudo chmod g+w " . $strTmpPath) if $bRemote;
         my $oMungeManifest = new pgBackRest::Manifest("$strTmpPath/backup.manifest");
         $oMungeManifest->remove(MANIFEST_SECTION_TARGET_FILE, MANIFEST_TARGET_PGDATA . '/' . DB_FILE_PGVERSION, 'checksum');
         $oMungeManifest->save();
@@ -748,9 +748,9 @@ sub run
         $strTmpPath = $oHostBackup->repoPath() . '/temp/' .$self->stanza() . '.tmp';
 
         testPathMove($oHostBackup->repoPath() . '/backup/' . $self->stanza() . "/${strBackup}", $strTmpPath);
-        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}");
+        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}") if $bRemote;
 
-        executeTest("sudo chmod g+w " . $strTmpPath);
+        executeTest("sudo chmod g+w " . $strTmpPath) if $bRemote;
         $oMungeManifest = new pgBackRest::Manifest("$strTmpPath/" . FILE_MANIFEST);
         $oMungeManifest->set(MANIFEST_SECTION_TARGET_FILE, MANIFEST_TARGET_PGDATA . '/badchecksum.txt', 'checksum', 'bogus');
         $oMungeManifest->save();
@@ -785,7 +785,7 @@ sub run
         $strTmpPath = $oHostBackup->repoPath() . '/temp/' . $self->stanza() . '.tmp';
 
         testPathMove($oHostBackup->repoPath() . '/backup/' . $self->stanza() . "/${strBackup}", $strTmpPath);
-        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}");
+        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}") if $bRemote;
 
         $strBackup = $oHostBackup->backup(
             $strType, 'cannot resume - new diff',
@@ -799,7 +799,7 @@ sub run
         $strTmpPath = $oHostBackup->repoPath() . '/temp/' . $self->stanza() . '.tmp';
 
         testPathMove($oHostBackup->repoPath() . '/backup/' . $self->stanza() . "/${strBackup}", $strTmpPath);
-        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}");
+        executeTest('sudo chown -R ' . BACKREST_USER . " ${strTmpPath}") if $bRemote;
 
         $strBackup = $oHostBackup->backup(
             $strType, 'cannot resume - disabled / no repo link',
